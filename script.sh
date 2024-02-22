02261d64fdaf557647f48a77752f46a611521d74
Leonardo Bonetti <leonardofbonetti@gmail.com>
2024-02-22 13:59:17 -0300

    alteracao do script

--- script.sh
+++ script.sh
@@ -2,17 +2,12 @@
 
 clean_files()
 {
-    if [$clean_tries -eq 2]; then
-        echo -e "ERRO!\n Nao foi possivel apagar os arquivos.\n Abortando operacao"
-        return 1
-    fi
     if [ "$ systemctl status httpd" -eq 0 ]; then 
         echo "HTTPD encontrado, desisnstalando"
         yum remove -y "httpd*"
         userdel apache
         rm -Rf /var/www /etc/httpd /usr/lib/httpd
         if [ "$ (systemctl stattus httpd)" -eq 1 ]; then
-            clean_tries = clean_tries + 1
             clean_files
         else
             echo "Arquivo removido com sucesso!"
@@ -22,7 +17,6 @@ clean_files()
 
 install_httpd()
 {
-    $clean_tries = 0
     systemctl start httpd
     if [ "$ (systemctl stattus httpd)" -eq 0 ]; then
         echo "HTTPD encontrado, desinstalando"
@@ -35,19 +29,19 @@ install_httpd()
         echo "HTTPD nao instalado, iniciando download"
         yum install -y httpd
         systemctl start httpd
-         echo "HTTPD instalado com sucesso"
+        echo "HTTPD instalado com sucesso"
     fi
 }
 
 install_php()
 {
     echo "Instalando PHP"
-    yum install -y php php-cli php-gd php-common
+    yum install -y php php-cli
 }
 
 vhost_creation()
 {
-    if [ "$ (nano /etc/httpd/conf.d/"$HOSTNAME".com.conf)" ]; then
+    if [ "$ (nano /etc/httpd/conf.d/"$HOSTNAME".com.conf)" -eq 0 ]; then
         echo -e "<VirtualHost *:80>\n   ServerName www.$HOSTNAME.com\n   ServerAlias $HOSTNAME.com\n   DocumentRoot "/var/www/html/$HOSTNAME.com"\n</VirtualHost>" >> /etc/httpd/conf.d/"$HOSTNAME".com.conf
     else
         rm -f /etc/httpd/conf.d/"$HOSTNAME".com
@@ -60,18 +54,12 @@ vhost_creation()
 search_cp_dir()
 {
     mkdir /var/www/html/"$HOSTNAME".com
-    chown -R apache:apache /var/www/html/"$HOSTNAME"
+    chown -R apache:apache /var/www/html/"$HOSTNAME".com
     vhost_creation
     mkdir /etc/content
     cp /etc/content/ /var/www/html/"$HOSTNAME".com
-    if [ "$ (find /etc/content/ -empty)" ]; then
-        echo "Arquivo Vazio"
-        clean_files
-        return 1
-    else
-        echo "Iniciando Copia"
-        cp /etc/content/. /var/www/html/"$HOSTNAME".com
-    fi
+    echo "Iniciando Copia"
+    cp /etc/content/. /var/www/html/"$HOSTNAME".com
 }
 
 firewalld_config()
@@ -79,10 +67,10 @@ firewalld_config()
     firewall-cmd --permanent --zone=public --add-port=80/tcp
     systemctl restart firewalld
     sed -i "s/$HOSTNAME/www."$HOSTNAME".com/g" /etc/hosts
+    systemctl restart httpd
 }
 
 install_httpd
 install_php
-vhost_creation
 search_cp_dir
 firewalld_config
