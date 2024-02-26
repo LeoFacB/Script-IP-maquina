fc56ceacaf5453b1f00a46440c95f5c868a2fa9b
Leonardo Bonetti <leonardofbonetti@gmail.com>
2024-02-26 09:49:00 -0300

    Atualização do Css e do script

--- index.php
+++ index.php
@@ -15,41 +15,39 @@
 
 
     <body>
-        <h2>Qual é o ip da máquina?</h2>
+        <div class="titulo">
+            <h1 style="color:white">Qual é o ip da máquina?</h1>
+        </div>
         <?php
-            $ip= shell_exec("hostname -i")
+            $ip= shell_exec("hostname -I")
         ?>
 
-        <div class="Background"></div>
+        <div class="caixa">
+            <!-- Button trigger modal -->
+            <button type="button" id="botaoModal" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ipModal">
+            Descubra o ip
+            </button>
 
+            <!-- Modal -->
+            <div class="modal fade" id="ipModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">O Ip da máquina é:</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <h1> <?php echo $ip;?> </h1>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                </div>
+                </div>
+            </div>
+            </div>
 
-        <!-- Button trigger modal -->
-        <button type="button" id="botaoModal" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ipModal">
-        Descubra o ip
-        </button>
-
-        <!-- Modal -->
-        <div class="modal fade" id="ipModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">O Ip da máquina é:</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <h1> <?php echo $ip;?> </h1>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
-            </div>
-            </div>
         </div>
-        </div>
 
-        
-        
-        
-        
         
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- script.sh
+++ script.sh
@@ -1,76 +1,43 @@
 #! /bin/bash
 
-clean_files()
-{
-    if [ "$ systemctl status httpd" -eq 0 ]; then 
-        echo "HTTPD encontrado, desisnstalando"
-        yum remove -y "httpd*"
-        userdel apache
-        rm -Rf /var/www /etc/httpd /usr/lib/httpd
-        if [ "$ (systemctl stattus httpd)" -eq 1 ]; then
-            clean_files
-        else
-            echo "Arquivo removido com sucesso!"
-        fi
-    fi
-}
-
-install_httpd()
+install_httpd_php()
 {
     systemctl start httpd
-    if [ "$ (systemctl stattus httpd)" -eq 0 ]; then
-        echo "HTTPD encontrado, desinstalando"
+    systemctl status httpd
+    if [ $? -eq 0 ]; then
         yum remove -y "httpd*"
         userdel apache
         rm -Rf /var/www /etc/httpd /usr/lib/httpd
-        echo "HTTPD removido com sucesso!"
         install_httpd
     else
-        echo "HTTPD nao instalado, iniciando download"
         yum install -y httpd
+        yum install -y php php-cli
         systemctl start httpd
-        echo "HTTPD instalado com sucesso"
     fi
-}
-
-install_php()
-{
-    echo "Instalando PHP"
-    yum install -y php php-cli
 }
 
 vhost_creation()
 {
-    if [ "$ (nano /etc/httpd/conf.d/"$HOSTNAME".com.conf)" -eq 0 ]; then
-        echo -e "<VirtualHost *:80>\n   ServerName www.$HOSTNAME.com\n   ServerAlias $HOSTNAME.com\n   DocumentRoot "/var/www/html/$HOSTNAME.com"\n</VirtualHost>" >> /etc/httpd/conf.d/"$HOSTNAME".com.conf
-    else
-        rm -f /etc/httpd/conf.d/"$HOSTNAME".com
-        nano -f /etc/httpd/conf.d/"$HOSTNAME".com
-        echo -e "<VirtualHost *:80>\n   ServerName www.$HOSTNAME.com\n   ServerAlias $HOSTNAME.com\n   DocumentRoot "/var/www/html/$HOSTNAME.com"\n</VirtualHost>" >> /etc/httpd/conf.d/"$HOSTNAME".com.conf
-    fi
-    systemctl restart httpd.service
+    echo -e "<VirtualHost *:80>\n   ServerName www.$HOSTNAME.com\n   DocumentRoot "/var/www/html/$HOSTNAME.com"\n</VirtualHost>" >> /etc/httpd/conf.d/"$HOSTNAME".com.conf
 }
 
-search_cp_dir()
+create_dir()
 {
     mkdir /var/www/html/"$HOSTNAME".com
     chown -R apache:apache /var/www/html/"$HOSTNAME".com
-    vhost_creation
     mkdir /etc/content
-    cp /etc/content/ /var/www/html/"$HOSTNAME".com
-    echo "Iniciando Copia"
     cp /etc/content/. /var/www/html/"$HOSTNAME".com
 }
 
 firewalld_config()
 {
     firewall-cmd --permanent --zone=public --add-port=80/tcp
-    systemctl restart firewalld
+    firewall-cmd --reload
     sed -i "s/$HOSTNAME/www."$HOSTNAME".com/g" /etc/hosts
     systemctl restart httpd
 }
 
-install_httpd
-install_php
-search_cp_dir
+install_httpd_php
+create_dir
+vhost_creation
 firewalld_config
